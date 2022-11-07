3d775fc240b166ccf5910ef9bc18b6d8bf607943
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2022-11-07 14:24:55 +0900

    Initial Commit

---
+++ FileRelationship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileRelationship
+//
+//  Created by 小林春 on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
